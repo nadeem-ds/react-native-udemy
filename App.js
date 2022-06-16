37ea997f34cf422fc166f38762e67e14fa8390db
Nadeem <anwarnadeem2010@gmail.com>
2022-06-16 17:22:31 +0530

    first basics

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.dummtText}>Nadeem Here!!</Text>
+      <Text style={styles.dummtText}>Welcoem to React Native Course😊</Text>
+      <Button
+        title="Click me to Submit👍"
+        onPress={() => alert("you submitted the form✔✔")}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummtText: {
+    margin: 20,
+    borderWidth: 2,
+    padding: 10,
+    backgroundColor: "grey",
   },
 });
