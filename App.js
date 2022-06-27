3233c5373bad1a2d0e0c14ee3495e39a6d5806f9
Nadeem <anwarnadeem2010@gmail.com>
2022-06-27 17:28:52 +0530

    Text input validation

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import { StyleSheet,  View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <StartGameScreen />
     </View>
   );
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,9 +4,15 @@ import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
-      <Text>StartGameScreen</Text>
-      <TextInput placeholder="Enter Text" />
+    <View style={styles.inputContainer}>
+      <Text>Start the guess app </Text>
+      <TextInput
+        style={styles.textInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton children="Reset" />
       <PrimaryButton children="Confirm" />
     </View>
@@ -15,4 +21,24 @@ const StartGameScreen = () => {
 
 export default StartGameScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 25,
+    borderRadius: 16,
+    padding: 16,
+    backgroundColor: "#72063c",
+    elevation: 20,
+  },
+  textInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
